83417b52071c4eda8d952515ad4d5e14131c6be9
Pplum09 <ir3@umbc.edu>
2016-06-29 03:01:31 -0400

    add option to process.ENV || 3000 option

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+app.set('port', (process.env.PORT || 3000));
 var users = [];
 
 app.get('/', function(req, res){
@@ -112,7 +113,7 @@ io.on('connection', function(socket){
     });
 });
 
-http.listen(3000, function(){
+http.listen(app.get('port'), function(){
     console.log('listening on *:3000');
 });
 
